9122580d313b1c9987abe482d70aa2fd310527f6
andrewjpage <andrewjpage@gmail.com>
2017-09-08 09:49:05 +0100

    add dist zilla

---
+++ lib/Abricate.pm
@@ -0,0 +1,10 @@
+package Abricate;
+# ABSTRACT: Mass screening of contigs for antimicrobial and virulence genes
+
+=head1 SYNOPSIS
+
+Mass screening of contigs for antimicrobial and virulence genes
+
+=cut
+
+1;
